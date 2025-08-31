cccdc234c549a94ad05a948b9256722b37aa4e5a
Erik Willem Domogalla <ed67qowe@tu-freiberg.de>
2025-08-31 16:52:42 +0200

    mehrere Simulationen und anpassung Skripte
    Hinzufügen OpenSmoke Reaktionsmechanismus
    Parameterstudie CO2
    Korrektur Plots

--- Scripts/Auswertungen/Vergleich_ChemistrySet/Auswertung_neu.py
+++ Scripts/Auswertungen/Vergleich_ChemistrySet/Auswertung_neu.py
@@ -24,8 +24,9 @@ color2 = '#7E9680'
 color3 = '#B3B3B3'
 color4 = '#BC6C25'
 color5 = "#960B0B" 
+color6 = "#077B1A"
 
-colors = [color1,color2,color3,color4,color5]
+colors = [color1,color2,color3,color4,color5, color6]
 #colors = sb.color_palette("muted", 5)
 
 # Set the working directory to the location of the script
@@ -61,102 +62,122 @@ df_co2_aramco_pfr = pd.read_excel('Daten/Aramco_CO2.xlsm', sheet_name='2.soln_no
 df_co2_gri_pfr = pd.read_excel('Daten/GRI_CO2.xlsm', sheet_name= '2.soln_no_1_PFRC2', decimal=',')
 df_co2_atr_pfr = pd.read_excel('Daten/ATR_CO2.xlsm', sheet_name= '2.soln_no_1_PFRC2', decimal=',')
 df_co2_nuig_pfr = pd.read_excel('Daten/NUIG_CO2.xlsm', sheet_name= '2.soln_no_1_PFRC2', decimal=',')
+df_co2_smoke_pfr = pd.read_excel('Daten/OpenSmoke_CO2.xlsm', sheet_name= '2.soln_no_1_PFRC2', decimal=',')
 
 df_no_co2_aramco_pfr = pd.read_excel('Daten/Aramco_keinCO2.xlsm', sheet_name='2.soln_no_1_PFRC2', decimal=',')
 df_no_co2_gri_pfr = pd.read_excel('Daten/GRI_keinCO2.xlsm', sheet_name= '2.soln_no_1_PFRC2', decimal=',')
 df_no_co2_atr_pfr = pd.read_excel('Daten/ATR_keinCO2.xlsm', sheet_name= '2.soln_no_1_PFRC2', decimal=',')
 df_no_co2_nuig_pfr = pd.read_excel('Daten/NUIG_keinCO2.xlsm', sheet_name= '2.soln_no_1_PFRC2', decimal=',')
+df_no_co2_smoke_pfr = pd.read_excel('Daten/OpenSmoke_keinCO2.xlsm', sheet_name= '2.soln_no_1_PFRC2', decimal=',')
 #%% Arrays CO2
 dist_pfr_aramco_co2 = df_co2_aramco_pfr['Distance_PFRC2_(m)']
 dist_pfr_gri_co2 = df_co2_gri_pfr['Distance_PFRC2_(m)']
 dist_pfr_atr_co2 = df_co2_atr_pfr['Distance_PFRC2_(m)']
 dist_pfr_nuig_co2 = df_co2_nuig_pfr['Distance_PFRC2_(m)']
+dist_pfr_smoke_co2 = df_co2_nuig_pfr['Distance_PFRC2_(m)']
 
 temp_pfr_aramco_co2 = df_co2_aramco_pfr[' Temperature_PFRC2_(K)']
 temp_pfr_gri_co2 = df_co2_gri_pfr[' Temperature_PFRC2_(K)']
 temp_pfr_atr_co2 = df_co2_atr_pfr[' Temperature_PFRC2_(K)']
 temp_pfr_nuig_co2 = df_co2_nuig_pfr[' Temperature_PFRC2_(K)']
+temp_pfr_smoke_co2 = df_co2_nuig_pfr[' Temperature_PFRC2_(K)']
 
 x_H2_pfr_aramco_co2 = df_co2_aramco_pfr[' Mole_fraction_H2_PFRC2_()']
 x_H2_pfr_gri_co2 = df_co2_gri_pfr[' Mole_fraction_H2_PFRC2_()']
 x_H2_pfr_atr_co2 = df_co2_atr_pfr[' Mole_fraction_H2_PFRC2_()']
 x_H2_pfr_nuig_co2 = df_co2_nuig_pfr[' Mole_fraction_H2_PFRC2_()']
+x_H2_pfr_smoke_co2 = df_co2_nuig_pfr[' Mole_fraction_H2_PFRC2_()']
 
 x_H2O_pfr_aramco_co2 = df_co2_aramco_pfr[' Mole_fraction_H2O_PFRC2_()']
 x_H2O_pfr_gri_co2 = df_co2_gri_pfr[' Mole_fraction_H2O_PFRC2_()']
 x_H2O_pfr_atr_co2 = df_co2_atr_pfr[' Mole_fraction_H2O_PFRC2_()']
 x_H2O_pfr_nuig_co2 = df_co2_nuig_pfr[' Mole_fraction_H2O_PFRC2_()']
+x_H2O_pfr_smoke_co2 = df_co2_nuig_pfr[' Mole_fraction_H2O_PFRC2_()']
 
 x_CO_pfr_aramco_co2 = df_co2_aramco_pfr[' Mole_fraction_CO_PFRC2_()']
 x_CO_pfr_gri_co2 = df_co2_gri_pfr[' Mole_fraction_CO_PFRC2_()']
 x_CO_pfr_atr_co2 = df_co2_atr_pfr[' Mole_fraction_CO_PFRC2_()']
 x_CO_pfr_nuig_co2 = df_co2_nuig_pfr[' Mole_fraction_CO_PFRC2_()']
+x_CO_pfr_smoke_co2 = df_co2_nuig_pfr[' Mole_fraction_CO_PFRC2_()']
 
 x_CO2_pfr_aramco_co2 = df_co2_aramco_pfr[' Mole_fraction_CO2_PFRC2_()']
 x_CO2_pfr_gri_co2 = df_co2_gri_pfr[' Mole_fraction_CO2_PFRC2_()']
 x_CO2_pfr_atr_co2 = df_co2_atr_pfr[' Mole_fraction_CO2_PFRC2_()']
 x_CO2_pfr_nuig_co2 = df_co2_nuig_pfr[' Mole_fraction_CO2_PFRC2_()']
+x_CO2_pfr_smoke_co2 = df_co2_nuig_pfr[' Mole_fraction_CO2_PFRC2_()']
 
 x_CH4_pfr_aramco_co2 = df_co2_aramco_pfr[' Mole_fraction_CH4_PFRC2_()']
 x_CH4_pfr_gri_co2 = df_co2_gri_pfr[' Mole_fraction_CH4_PFRC2_()']
 x_CH4_pfr_atr_co2 = df_co2_atr_pfr[' Mole_fraction_CH4_PFRC2_()']
 x_CH4_pfr_nuig_co2 = df_co2_nuig_pfr[' Mole_fraction_CH4_PFRC2_()']
+x_CH4_pfr_smoke_co2 = df_co2_nuig_pfr[' Mole_fraction_CH4_PFRC2_()']
 
 x_unburned_pfr_aramco_co2 = df_co2_aramco_pfr[' Unburned_hydrocarbons_PFRC2_(ppm)']
 x_unburned_pfr_gri_co2 = df_co2_gri_pfr[' Unburned_hydrocarbons_PFRC2_(ppm)']
 x_unburned_pfr_atr_co2 = df_co2_atr_pfr[' Unburned_hydrocarbons_PFRC2_(ppm)']
 x_unburned_pfr_nuig_co2 = df_co2_nuig_pfr[' Unburned_hydrocarbons_PFRC2_(ppm)']
+x_unburned_pfr_smoke_co2 = df_co2_nuig_pfr[' Unburned_hydrocarbons_PFRC2_(ppm)']
 
 x_test_pfr_aramco_co2 = df_co2_aramco_pfr[' Mole_fraction_N2_PFRC2_()']
 x_test_pfr_gri_co2 = df_co2_gri_pfr[' Mole_fraction_N2_PFRC2_()']
 x_test_pfr_atr_co2 = df_co2_atr_pfr[' Mole_fraction_N2_PFRC2_()']
 x_test_pfr_nuig_co2 = df_co2_nuig_pfr[' Mole_fraction_N2_PFRC2_()']
-#%% Arrays kein CO2
+x_test_pfr_smoke_co2 = df_co2_nuig_pfr[' Mole_fraction_N2_PFRC2_()']#%% Arrays kein CO2
+
 dist_pfr_aramco_no_co2 = df_no_co2_aramco_pfr['Distance_PFRC2_(m)']
 dist_pfr_gri_no_co2 = df_no_co2_gri_pfr['Distance_PFRC2_(m)']
 dist_pfr_atr_no_co2 = df_no_co2_atr_pfr['Distance_PFRC2_(m)']
 dist_pfr_nuig_no_co2 = df_no_co2_nuig_pfr['Distance_PFRC2_(m)']
+dist_pfr_smoke_no_co2 = df_no_co2_nuig_pfr['Distance_PFRC2_(m)']
 
 temp_pfr_aramco_no_co2 = df_no_co2_aramco_pfr[' Temperature_PFRC2_(K)']
 temp_pfr_gri_no_co2 = df_no_co2_gri_pfr[' Temperature_PFRC2_(K)']
 temp_pfr_atr_no_co2 = df_no_co2_atr_pfr[' Temperature_PFRC2_(K)']
 temp_pfr_nuig_no_co2 = df_no_co2_nuig_pfr[' Temperature_PFRC2_(K)']
+temp_pfr_smoke_no_co2 = df_no_co2_nuig_pfr[' Temperature_PFRC2_(K)']
 
 x_H2_pfr_aramco_no_co2 = df_no_co2_aramco_pfr[' Mole_fraction_H2_PFRC2_()']
 x_H2_pfr_gri_no_co2 = df_no_co2_gri_pfr[' Mole_fraction_H2_PFRC2_()']
 x_H2_pfr_atr_no_co2 = df_no_co2_atr_pfr[' Mole_fraction_H2_PFRC2_()']
 x_H2_pfr_nuig_no_co2 = df_no_co2_nuig_pfr[' Mole_fraction_H2_PFRC2_()']
+x_H2_pfr_smoke_no_co2 = df_no_co2_nuig_pfr[' Mole_fraction_H2_PFRC2_()']
 
 x_H2O_pfr_aramco_no_co2 = df_no_co2_aramco_pfr[' Mole_fraction_H2O_PFRC2_()']
 x_H2O_pfr_gri_no_co2 = df_no_co2_gri_pfr[' Mole_fraction_H2O_PFRC2_()']
 x_H2O_pfr_atr_no_co2 = df_no_co2_atr_pfr[' Mole_fraction_H2O_PFRC2_()']
 x_H2O_pfr_nuig_no_co2 = df_no_co2_nuig_pfr[' Mole_fraction_H2O_PFRC2_()']
+x_H2O_pfr_smoke_no_co2 = df_no_co2_nuig_pfr[' Mole_fraction_H2O_PFRC2_()']
 
 x_CO_pfr_aramco_no_co2 = df_no_co2_aramco_pfr[' Mole_fraction_CO_PFRC2_()']
 x_CO_pfr_gri_no_co2 = df_no_co2_gri_pfr[' Mole_fraction_CO_PFRC2_()']
 x_CO_pfr_atr_no_co2 = df_no_co2_atr_pfr[' Mole_fraction_CO_PFRC2_()']
 x_CO_pfr_nuig_no_co2 = df_no_co2_nuig_pfr[' Mole_fraction_CO_PFRC2_()']
+x_CO_pfr_smoke_no_co2 = df_no_co2_nuig_pfr[' Mole_fraction_CO_PFRC2_()']
 
 x_CO2_pfr_aramco_no_co2 = df_no_co2_aramco_pfr[' Mole_fraction_CO2_PFRC2_()']
 x_CO2_pfr_gri_no_co2 = df_no_co2_gri_pfr[' Mole_fraction_CO2_PFRC2_()']
 x_CO2_pfr_atr_no_co2 = df_no_co2_atr_pfr[' Mole_fraction_CO2_PFRC2_()']
 x_CO2_pfr_nuig_no_co2 = df_no_co2_nuig_pfr[' Mole_fraction_CO2_PFRC2_()']
+x_CO2_pfr_smoke_no_co2 = df_no_co2_nuig_pfr[' Mole_fraction_CO2_PFRC2_()']
 
 x_CH4_pfr_aramco_no_co2 = df_no_co2_aramco_pfr[' Mole_fraction_CH4_PFRC2_()']
 x_CH4_pfr_gri_no_co2 = df_no_co2_gri_pfr[' Mole_fraction_CH4_PFRC2_()']
 x_CH4_pfr_atr_no_co2 = df_no_co2_atr_pfr[' Mole_fraction_CH4_PFRC2_()']
 x_CH4_pfr_nuig_no_co2 = df_no_co2_nuig_pfr[' Mole_fraction_CH4_PFRC2_()']
+x_CH4_pfr_smoke_no_co2 = df_no_co2_nuig_pfr[' Mole_fraction_CH4_PFRC2_()']
 
 x_unburned_pfr_aramco_no_co2 = df_no_co2_aramco_pfr[' Unburned_hydrocarbons_PFRC2_(ppm)']
 x_unburned_pfr_gri_no_co2 = df_no_co2_gri_pfr[' Unburned_hydrocarbons_PFRC2_(ppm)']
 x_unburned_pfr_atr_no_co2 = df_no_co2_atr_pfr[' Unburned_hydrocarbons_PFRC2_(ppm)']
 x_unburned_pfr_nuig_no_co2 = df_no_co2_nuig_pfr[' Unburned_hydrocarbons_PFRC2_(ppm)']
+x_unburned_pfr_smoke_no_co2 = df_no_co2_nuig_pfr[' Unburned_hydrocarbons_PFRC2_(ppm)']
 
 x_test_pfr_aramco_no_co2 = df_no_co2_aramco_pfr[' Mole_fraction_N2_PFRC2_()']
 x_test_pfr_gri_no_co2 = df_no_co2_gri_pfr[' Mole_fraction_N2_PFRC2_()']
 x_test_pfr_atr_no_co2 = df_no_co2_atr_pfr[' Mole_fraction_N2_PFRC2_()']
 x_test_pfr_nuig_no_co2 = df_no_co2_nuig_pfr[' Mole_fraction_N2_PFRC2_()']
-#%% Plots 
+x_test_pfr_smoke_no_co2 = df_no_co2_nuig_pfr[' Mole_fraction_N2_PFRC2_()']#%% Plots 
+
 fig, axs = plt.subplots(2, 2, figsize=(7, 7))
 
 # H2
@@ -164,6 +185,7 @@ axs[0, 0].plot(dist_pfr_aramco_no_co2, x_H2_pfr_aramco_no_co2, label='aramco', c
 axs[0, 0].plot(dist_pfr_gri_no_co2, x_H2_pfr_gri_no_co2, label='gri', color=colors[1])
 axs[0, 0].plot(dist_pfr_atr_no_co2, x_H2_pfr_atr_no_co2, label='atr', color=colors[2])
 axs[0, 0].plot(dist_pfr_nuig_no_co2, x_H2_pfr_nuig_no_co2, label='nuig', color=colors[3])
+axs[0, 0].plot(dist_pfr_smoke_no_co2, x_H2_pfr_smoke_no_co2, label='OpenSmoke', color=colors[4])
 axs[0, 0].set_xlabel("Reaktorlänge (m)")
 axs[0, 0].set_ylabel("Massenanteil H₂")
 axs[0, 0].set_title("H₂")
@@ -175,6 +197,7 @@ axs[0, 1].plot(dist_pfr_aramco_no_co2, x_CH4_pfr_aramco_no_co2, label='aramco', 
 axs[0, 1].plot(dist_pfr_gri_no_co2, x_CH4_pfr_gri_no_co2, label='gri', color=colors[1])
 axs[0, 1].plot(dist_pfr_atr_no_co2, x_CH4_pfr_atr_no_co2, label='atr', color=colors[2])
 axs[0, 1].plot(dist_pfr_nuig_no_co2, x_CH4_pfr_nuig_no_co2, label='nuig', color=colors[3])
+axs[0, 1].plot(dist_pfr_smoke_no_co2, x_CH4_pfr_smoke_no_co2, label='OpenSmoke', color=colors[4])
 axs[0, 1].set_xlabel("Reaktorlänge (m)")
 axs[0, 1].set_ylabel("Massenanteil CH₄")
 axs[0, 1].set_title("CH₄")
@@ -186,6 +209,7 @@ axs[1, 0].plot(dist_pfr_aramco_no_co2, x_CO2_pfr_aramco_no_co2, label='aramco', 
 axs[1, 0].plot(dist_pfr_gri_no_co2, x_CO2_pfr_gri_no_co2, label='gri', color=colors[1])
 axs[1, 0].plot(dist_pfr_atr_no_co2, x_CO2_pfr_atr_no_co2, label='atr', color=colors[2])
 axs[1, 0].plot(dist_pfr_nuig_no_co2, x_CO2_pfr_nuig_no_co2, label='nuig', color=colors[3])
+axs[1, 0].plot(dist_pfr_smoke_no_co2, x_CO2_pfr_smoke_no_co2, label='OpenSmoke', color=colors[4])
 axs[1, 0].set_xlabel("Reaktorlänge (m)")
 axs[1, 0].set_ylabel("Massenanteil CO₂")
 axs[1, 0].set_title("CO₂")
@@ -197,6 +221,7 @@ axs[1, 1].plot(dist_pfr_aramco_no_co2, x_CO_pfr_aramco_no_co2, label='aramco', c
 axs[1, 1].plot(dist_pfr_gri_no_co2, x_CO_pfr_gri_no_co2, label='gri', color=colors[1])
 axs[1, 1].plot(dist_pfr_atr_no_co2, x_CO_pfr_atr_no_co2, label='atr', color=colors[2])
 axs[1, 1].plot(dist_pfr_nuig_no_co2, x_CO_pfr_nuig_no_co2, label='nuig', color=colors[3])
+axs[1, 1].plot(dist_pfr_smoke_no_co2, x_CO_pfr_smoke_no_co2, label='OpenSmoke', color=colors[4])
 axs[1, 1].set_xlabel("Reaktorlänge (m)")
 axs[1, 1].set_ylabel("Massenanteil CO")
 axs[1, 1].set_title("CO")
@@ -230,6 +255,7 @@ axs[0, 0].plot(dist_pfr_aramco_co2, x_CO_pfr_aramco_co2, label='aramco', color=c
 axs[0, 0].plot(dist_pfr_gri_co2, x_CO_pfr_gri_co2, label='gri', color=colors[1])
 axs[0, 0].plot(dist_pfr_atr_co2, x_CO_pfr_atr_co2, label='atr', color=colors[2])
 axs[0, 0].plot(dist_pfr_nuig_co2, x_CO_pfr_nuig_co2, label='nuig', color=colors[3])
+axs[0, 0].plot(dist_pfr_smoke_co2, x_CO_pfr_smoke_co2, label='OpenSmoke', color=colors[4])
 axs[0, 0].set_xlabel("Reaktorlänge (m)")
 axs[0, 0].set_ylabel("Massenanteil CO")
 axs[0, 0].set_title("CO")
@@ -241,6 +267,7 @@ axs[0, 1].plot(dist_pfr_aramco_co2, x_CO2_pfr_aramco_co2, label='aramco', color=
 axs[0, 1].plot(dist_pfr_gri_co2, x_CO2_pfr_gri_co2, label='gri', color=colors[1])
 axs[0, 1].plot(dist_pfr_atr_co2, x_CO2_pfr_atr_co2, label='atr', color=colors[2])
 axs[0, 1].plot(dist_pfr_nuig_co2, x_CO2_pfr_nuig_co2, label='nuig', color=colors[3])
+axs[0, 1].plot(dist_pfr_smoke_co2, x_CO2_pfr_smoke_co2, label='OpenSmoke', color=colors[4])
 axs[0, 1].set_xlabel("Reaktorlänge (m)")
 axs[0, 1].set_ylabel("Massenanteil CO₂")
 axs[0, 1].set_title("CO₂")
@@ -252,6 +279,7 @@ axs[1, 0].plot(dist_pfr_aramco_co2, x_H2_pfr_aramco_co2, label='aramco', color=c
 axs[1, 0].plot(dist_pfr_gri_co2, x_H2_pfr_gri_co2, label='gri', color=colors[1])
 axs[1, 0].plot(dist_pfr_atr_co2, x_H2_pfr_atr_co2, label='atr', color=colors[2])
 axs[1, 0].plot(dist_pfr_nuig_co2, x_H2_pfr_nuig_co2, label='nuig', color=colors[3])
+axs[1, 0].plot(dist_pfr_smoke_co2, x_H2_pfr_smoke_co2, label='OpenSmoke', color=colors[4])
 axs[1, 0].set_xlabel("Reaktorlänge (m)")
 axs[1, 0].set_ylabel("Massenanteil H₂")
 axs[1, 0].set_title("H₂")
@@ -263,6 +291,7 @@ axs[1, 1].plot(dist_pfr_aramco_co2, x_CH4_pfr_aramco_co2, label='aramco', color=
 axs[1, 1].plot(dist_pfr_gri_co2, x_CH4_pfr_gri_co2, label='gri', color=colors[1])
 axs[1, 1].plot(dist_pfr_atr_co2, x_CH4_pfr_atr_co2, label='atr', color=colors[2])
 axs[1, 1].plot(dist_pfr_nuig_co2, x_CH4_pfr_nuig_co2, label='nuig', color=colors[3])
+axs[1, 1].plot(dist_pfr_smoke_co2, x_CH4_pfr_smoke_co2, label='OpenSmoke', color=colors[4])
 axs[1, 1].set_xlabel("Reaktorlänge (m)")
 axs[1, 1].set_ylabel("Massenanteil CH₄")
 axs[1, 1].set_title("CH₄")
@@ -310,11 +339,13 @@ gri_co2    = get_normalized_composition_strict(df_co2_gri_pfr, species_column_ma
 aramco_co2 = get_normalized_composition_strict(df_co2_aramco_pfr, species_column_map)
 nuig_co2   = get_normalized_composition_strict(df_co2_nuig_pfr, species_column_map)
 atr_co2    = get_normalized_composition_strict(df_co2_atr_pfr, species_column_map)
+smoke_co2    = get_normalized_composition_strict(df_co2_smoke_pfr, species_column_map)
 
 gri_no_co2    = get_normalized_composition_strict(df_no_co2_gri_pfr, species_column_map)
 aramco_no_co2 = get_normalized_composition_strict(df_no_co2_aramco_pfr, species_column_map)
 nuig_no_co2   = get_normalized_composition_strict(df_no_co2_nuig_pfr, species_column_map)
 atr_no_co2    = get_normalized_composition_strict(df_no_co2_atr_pfr, species_column_map)
+smoke_no_co2    = get_normalized_composition_strict(df_no_co2_smoke_pfr, species_column_map)
 
 vergleich_species = ["H2", "CO", "CH4", "CO2"]
 
@@ -330,17 +361,20 @@ df_exp_data["ATR_CO2"]       = atr_co2.reindex(vergleich_species)
 df_exp_data["NUIG_noCO2"]    = nuig_no_co2.reindex(vergleich_species)
 df_exp_data["NUIG_CO2"]      = nuig_co2.reindex(vergleich_species)
 
+df_exp_data["Smoke_noCO2"]    = smoke_no_co2.reindex(vergleich_species)
+df_exp_data["Smoke_CO2"]      = smoke_co2.reindex(vergleich_species)
+
 df_to_table(df_exp_data, 
-            columns = ["kein CO2 Exp", "GRI_noCO2", "ARAMCO_noCO2", "ATR_noCO2", "NUIG_noCO2"],
-            rounding = [3,3,3,3,3],
+            columns = ["kein CO2 Exp", "GRI_noCO2", "ARAMCO_noCO2", "ATR_noCO2", "NUIG_noCO2", "Smoke_noCO2"],
+            rounding = [3,3,3,3,3,3],
             latex = True,
             filepath = "Tabellen/Tabelle_Vergleich_keinCO2.tex",
             decimal_sep = ","
             )
 
 df_to_table(df_exp_data, 
-            columns = ["CO2 Exp", "GRI_CO2", "ARAMCO_CO2", "ATR_CO2", "NUIG_CO2"],
-            rounding = [3,3,3,3,3],
+            columns = ["CO2 Exp", "GRI_CO2", "ARAMCO_CO2", "ATR_CO2", "NUIG_CO2", "Smoke_CO2"],
+            rounding = [3,3,3,3,3,3],
             latex = True,
             filepath = "Tabellen/Tabelle_Vergleich_CO2.tex",
             decimal_sep = ","
@@ -349,8 +383,8 @@ df_to_table(df_exp_data,
 
 # Spalten gruppieren: jeweils "kein CO2" und "CO2" separat plotten
 gruppen = [
-    ["kein CO2 Exp", "GRI_noCO2", "ARAMCO_noCO2", "ATR_noCO2", "NUIG_noCO2"],
-    ["CO2 Exp",      "GRI_CO2",   "ARAMCO_CO2",   "ATR_CO2",   "NUIG_CO2"]
+    ["kein CO2 Exp", "GRI_noCO2", "ARAMCO_noCO2", "ATR_noCO2", "NUIG_noCO2", "Smoke_noCO2"],
+    ["CO2 Exp",      "GRI_CO2",   "ARAMCO_CO2",   "ATR_CO2",   "NUIG_CO2", "Smoke_CO2"]
 ]
 titel = ["Vergleich ohne CO₂", "Vergleich mit CO₂"]
 
@@ -397,12 +431,14 @@ ax1.plot(dist_pfr_aramco_no_co2, temp_pfr_aramco_no_co2, color=colors[0], label=
 ax1.plot(dist_pfr_gri_no_co2, temp_pfr_gri_no_co2, color=colors[1], label='gri, Temperatur')
 ax1.plot(dist_pfr_atr_no_co2, temp_pfr_atr_no_co2, color=colors[2], label='atr, Temperatur')
 ax1.plot(dist_pfr_nuig_no_co2, temp_pfr_nuig_no_co2, color=colors[3], label='nuig, Temperatur')
+ax1.plot(dist_pfr_smoke_no_co2, temp_pfr_smoke_no_co2, color=colors[4], label='smoke, Temperatur')
 ax1.set_ylabel("Temperatur (K)")
 
 ax2.plot(dist_pfr_aramco_no_co2, x_CH4_pfr_aramco_no_co2, color=colors[0], label='aramco, CH4', linestyle = "--")
 ax2.plot(dist_pfr_gri_no_co2, x_CH4_pfr_gri_no_co2, color=colors[1], label='gri, CH4', linestyle = "--")
 ax2.plot(dist_pfr_atr_no_co2, x_CH4_pfr_atr_no_co2, color=colors[2], label='atr, CH4', linestyle = "--")
 ax2.plot(dist_pfr_nuig_no_co2, x_CH4_pfr_nuig_no_co2, color=colors[3], label='nuig, CH4', linestyle = "--")
+ax2.plot(dist_pfr_smoke_no_co2, x_CH4_pfr_smoke_no_co2, color=colors[4], label='smoke, CH4', linestyle = "--")
 ax2.set_ylabel("CH$_4$ Molenbruch")
 
 ax1.set_title("ohne CO₂")
@@ -421,12 +457,14 @@ ax3.plot(dist_pfr_aramco_co2, temp_pfr_aramco_co2, color=colors[0], label='aramc
 ax3.plot(dist_pfr_gri_co2, temp_pfr_gri_co2, color=colors[1], label='gri, Temperatur')
 ax3.plot(dist_pfr_atr_co2, temp_pfr_atr_co2, color=colors[2], label='atr, Temperatur')
 ax3.plot(dist_pfr_nuig_co2, temp_pfr_nuig_co2, color=colors[3], label='nuig, Temperatur')
+ax3.plot(dist_pfr_smoke_co2, temp_pfr_smoke_co2, color=colors[4], label='smoke, Temperatur')
 ax3.set_ylabel("Temperatur (K)")
 
 ax4.plot(dist_pfr_aramco_co2, x_CH4_pfr_aramco_co2, color=colors[0], label='aramco, CH4', linestyle = "--")
 ax4.plot(dist_pfr_gri_co2, x_CH4_pfr_gri_co2, color=colors[1], label='gri, CH4', linestyle = "--")
 ax4.plot(dist_pfr_atr_co2, x_CH4_pfr_atr_co2, color=colors[2], label='atr, CH4', linestyle = "--")
 ax4.plot(dist_pfr_nuig_co2, x_CH4_pfr_nuig_co2, color=colors[3], label='nuig, CH4', linestyle = "--")
+ax4.plot(dist_pfr_smoke_co2, x_CH4_pfr_smoke_co2, color=colors[4], label='smoke, CH4', linestyle = "--")
 ax4.set_ylabel("CH$_4$ Molenbruch")
 
 ax3.set_title("mit CO₂")
@@ -449,3 +487,17 @@ os.makedirs(dst_folder, exist_ok=True)
 
 # Inhalt von img nach ../Bericht/Bilder kopieren
 shutil.copytree(src_folder, dst_folder, dirs_exist_ok=True)
+
+plt.close("all")
+
+#plt.plot(dist_pfr_nuig_no_co2, temp_pfr_nuig_no_co2, color=colors[3], label='nuig, Temperatur')
+#plt.plot(dist_pfr_smoke_no_co2, temp_pfr_smoke_no_co2, color=colors[4], label='smoke, Temperatur')
+plt.plot(dist_pfr_nuig_no_co2, temp_pfr_nuig_no_co2 - temp_pfr_smoke_no_co2, color=colors[3], label='nuig, Temperatur')
+plt.legend()
+plt.grid()
+plt.show()
+
+print(temp_pfr_nuig_no_co2)
+print(temp_pfr_smoke_no_co2)
+
+print(temp_pfr_nuig_no_co2 == temp_pfr_smoke_no_co2)
